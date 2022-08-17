94e1b9d823d6b37fcb5a74411ea43bebdc45413f
Simay Gözde <simayygozde@gmail.com>
2022-08-17 14:06:29 +0300

    İnitial commit for app

---
+++ src/main/java/com/example/libraryApp/LibraryApplication.java
@@ -0,0 +1,14 @@
+package com.example.libraryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
